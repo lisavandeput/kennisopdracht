81e2ea5017d8ace039aa3be92c17c38e15bb85f4
lisavandeput <36765121+lisavandeput@users.noreply.github.com>
2018-02-24 17:48:31 +0100

    Add files via upload

--- test.js
+++ test.js
@@ -1,20 +1,61 @@
 var main = function ()
 {
+ 		var on = false;
+		$(document).on('click', '.icon-menu', function(){      
+    	if (!on) 
+			{ 
+				$('.menu').animate({ 
+					left: "0px"}, 200);
+				on = true;
+			}
+			
+			else 
+			{ 
+				$('.menu').animate({ 
+					left: "-285px"}, 200);
+				on = false;
+			}
+		});
+		
+		$('.home').click(function(){
+			$('.container').hide();
+		});
+		
+		$('.cv').click(function(){
+			$('.container').show();
+			$('#cv').show();
+			$('#foto').hide();
+		});
+		
+		$('.foto').click(function(){
+			$('.container').show();
+			$('#foto').show();
+			$('#cv').hide();
+		});
+    
+    $(function stay() {
+    	$('.menu li a').click(function stay() {            
+        $('.menu li a').removeClass('selected');
+        $(this).addClass('selected');                        
+    	});
+		});
+		
     $('.arrow-next').click(function()
     {
-        var currentSlide = $('.slide1');
+		 		var currentSlide = $('.slide1');
         var nextSlide = currentSlide.next();
         var currentDot = $('.active-dot');
         var nextDot = currentDot.next();
         
         if (nextSlide.length === 0)
         {
-            nextSlide = $('.slide').first();
+				 	  nextSlide = $('.slide').first();
             nextDot = $('.dot').first();
         }
         
-        currentSlide.fadeOut(600).removeClass('slide1');
-        nextSlide.fadeIn(600).addClass('slide1');
+        currentSlide.fadeOut(600, function(){
+				nextSlide.fadeIn(600).addClass('slide1');
+				}).removeClass('slide1');
         currentDot.removeClass('active-dot');
         nextDot.addClass('active-dot');
     });
@@ -32,11 +73,13 @@ var main = function ()
             prevDot = $('.dot').last();
         }
         
-        currentSlide.fadeOut(600).removeClass('slide1');
-        prevSlide.fadeIn(600).addClass('slide1');
+        currentSlide.fadeOut(600, function(){
+				prevSlide.fadeIn(600).addClass('slide1');
+				}).removeClass('slide1');
         currentDot.removeClass('active-dot');
         prevDot.addClass('active-dot');
     });
+		
 };
 
 $(document).ready(main);
